ea570300666566b03564df95e1e66b2c0fa8f074
donatty <qiqiom@gmail.com>
2026-07-16 11:54:02 +0700

    add sytex social

--- lib/questions/index.ts
+++ lib/questions/index.ts
@@ -2,6 +2,7 @@ import { Subject } from "@/types/profile";
 import { Question } from "@/types/question";
 import { mathQuestions } from "./math";
 import { englishQuestions } from "./english";
+import { socialQuestions } from "./social";
 
 // TODO: เติม thai.ts, science.ts, social.ts ตามโครงเดียวกันนี้
 const bank: Record<Subject, Question[]> = {
@@ -9,7 +10,7 @@ const bank: Record<Subject, Question[]> = {
   english: englishQuestions,
   thai: [],
   science: [],
-  social: [],
+  social: socialQuestions,
 };
 
 export function getQuestionsForSubject(subject: Subject): Question[] {
